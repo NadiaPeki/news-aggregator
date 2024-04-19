445c087a64f9381b2d4738a010cd8a3b7c243d75
Nadzeya Pekarskaya <napekarskaya@gmail.com>
2024-04-19 18:59:47 +0200

    deleted news API

--- src/components/FilterNewsPannel/FilterNewsPannel.jsx
+++ src/components/FilterNewsPannel/FilterNewsPannel.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { useLocalStorage } from 'usehooks-ts';
-import styles from './FilterNewsPannel.module.css'
+import styles from './FilterNewsPannel.module.css';
 
 function FilterNewsPannel({ onApplyFilters }) {
   const [filters, setFilters] = useLocalStorage('articleFilters', {
     category: '',
     source: '',
-    author: ''
+    author: '',
   });
 
   const handleChange = (e) => {
@@ -22,25 +22,45 @@ function FilterNewsPannel({ onApplyFilters }) {
     <div className={styles.filterPannel}>
       <h2 className={styles.filterHeader}>Filter Articles</h2>
       <div className={styles.filtersWrapper}>
-      <label className={styles.labelText}>
-        Category:
-        <input type="text" name="category" placeholder='e.g.music, art' className={styles.inputFilter} value={filters.category} onChange={handleChange} />
-      </label>
-      <label className={styles.labelText}>
-        Author:
-        <input type="text" name="author" placeholder="type the author's name" className={styles.inputFilter} value={filters.author} onChange={handleChange} />
+        <label className={styles.labelText}>
+          Category:
+          <input
+            type="text"
+            name="category"
+            placeholder="e.g.music, art"
+            className={styles.inputFilter}
+            value={filters.category}
+            onChange={handleChange}
+          />
         </label>
-      <label className={styles.labelTextCategory}>
-        Source:
-        <select name="source" className={styles.inputSelect} value={filters.source} onChange={handleChange}>
-          <option value="">All</option>
-          <option value="NewsAPI">NewsAPI</option>
-          <option value="New York Times">New York Times</option>
-          <option value="The Guardian">The Guardian</option>
-        </select>
-      </label>
-        <button className={styles.buttonFilter} onClick={handleApplyFilters}>Apply Filters</button>
-        </div>
+        <label className={styles.labelText}>
+          Author:
+          <input
+            type="text"
+            name="author"
+            placeholder="type the author's name"
+            className={styles.inputFilter}
+            value={filters.author}
+            onChange={handleChange}
+          />
+        </label>
+        <label className={styles.labelTextCategory}>
+          Source:
+          <select
+            name="source"
+            className={styles.inputSelect}
+            value={filters.source}
+            onChange={handleChange}>
+            <option value="">All</option>
+            {/* <option value="NewsAPI">NewsAPI</option> */}
+            <option value="New York Times">New York Times</option>
+            <option value="The Guardian">The Guardian</option>
+          </select>
+        </label>
+        <button className={styles.buttonFilter} onClick={handleApplyFilters}>
+          Apply Filters
+        </button>
+      </div>
     </div>
   );
 }
